dae80877c7fff9312b027df16e87c151061ec56e
mszahan <mszahan1@gmail.com>
2024-01-13 23:10:31 +0600

    added the search component with tailwind css

--- expore_react/interface/src/App.jsx
+++ expore_react/interface/src/App.jsx
@@ -1,14 +1,14 @@
 import {createRoot} from 'react-dom/client';
 import {BiArchive} from 'react-icons/bi'
+import Search from './components/Search';
+
 
 
 function App () {
     return(
-        <div className="app">
-            <h1 className='text-3xl'> <BiArchive/> Hola from app</h1>
-            <h2>
-                tailwindcss/forms
-            </h2>
+        <div className="App container mx-auto mt-3 font-thin">
+            <h1 className='text-3xl'> <BiArchive className='inline-block text-red-400'/> Your Appointment</h1>
+            <Search/>
         </div>
     )
 }
