e17c1fdd0fde5bf7074e4c4b286dac22d41735a2
mszahan <mszahan1@gmail.com>
2024-01-13 23:21:00 +0600

    added the add appointment component and the json data

--- expore_react/interface/src/App.jsx
+++ expore_react/interface/src/App.jsx
@@ -1,13 +1,15 @@
 import {createRoot} from 'react-dom/client';
 import {BiArchive} from 'react-icons/bi'
 import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
 
 
 
 function App () {
     return(
-        <div className="App container mx-auto mt-3 font-thin">
-            <h1 className='text-3xl'> <BiArchive className='inline-block text-red-400'/> Your Appointment</h1>
+        <div className="App container mx-auto mt-3 font-thin p-5">
+            <h1 className='text-3xl'> <BiArchive className='inline-block text-red-400 mb-3'/> Your Appointment</h1>
+            <AddAppointment/>
             <Search/>
         </div>
     )
