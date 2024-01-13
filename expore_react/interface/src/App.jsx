9e7990068abdc5193673d172554269db976fe4b6
mszahan <mszahan1@gmail.com>
2024-01-13 22:44:41 +0600

    installed and used tailwind css

--- expore_react/interface/src/App.jsx
+++ expore_react/interface/src/App.jsx
@@ -5,7 +5,10 @@ import {BiArchive} from 'react-icons/bi'
 function App () {
     return(
         <div className="app">
-            <h1> <BiArchive/> Hola from app</h1>
+            <h1 className='text-3xl'> <BiArchive/> Hola from app</h1>
+            <h2>
+                tailwindcss/forms
+            </h2>
         </div>
     )
 }
