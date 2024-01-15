86a7dd6487a82b9665339eb3dfba3bbc926e8460
mszahan <mszahan1@gmail.com>
2024-01-15 12:34:36 +0600

    added dynamic url for article detail page with useParam hook

--- expore_react/mernstack/src/pages/ArticlePage.jsx
+++ expore_react/mernstack/src/pages/ArticlePage.jsx
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+import articles from './article-content'
+
+
 const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is the article detail page</h1>
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map(para => (
+            <p>{para}</p>
+        ))}
+        
+        </>
     )
 }
 
