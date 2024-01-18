814673774f4156ad6848e23f4810f320c94864ca
mszahan <mszahan1@gmail.com>
2024-01-18 23:39:54 +0600

    explored the json and get and post request in express

--- expore_react/mernstack/backend/src/server.js
+++ expore_react/mernstack/backend/src/server.js
@@ -2,8 +2,19 @@ import express from 'express';
 
 const app = express()
 
-app.get('/hello', (req, res) => {
-    res.send('Hello');
+// app.get('/hello', (req, res) => {
+//     res.send('Hello');
+// });
+
+app.use(express.json());
+
+app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello ${name}`);
 });
 
 app.listen(8000, ()=> {
