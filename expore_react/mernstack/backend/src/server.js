0147f2dc1f34efe01b62fa3886c98447d5c421ef
mszahan <mszahan1@gmail.com>
2024-01-19 00:26:20 +0600

    added put request to updaqte the upvote

--- expore_react/mernstack/backend/src/server.js
+++ expore_react/mernstack/backend/src/server.js
@@ -1,21 +1,55 @@
 import express from 'express';
 
+let articleInfo = [
+    {
+        name: 'learn-rect',
+        upvotes:0
+    },
+    {
+        name: 'learn-node',
+        upvotes:0
+    },
+    {
+        name: 'mongodb',
+        upvotes:0
+    },
+]
+
 const app = express()
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const {name} = req.params;
+    const article = articleInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} articles now has ${article.upvotes} upvotes `);
+
+    } else {
+        res.send('That article does not exist');
+    }
+})
+
+
+
 
 // app.get('/hello', (req, res) => {
 //     res.send('Hello');
 // });
 
-app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
-});
 
-app.get('/hello/:name', (req, res) => {
-    const { name } = req.params;
-    res.send(`Hello ${name}`);
-});
+
+
+
+// app.post('/hello', (req, res) => {
+//     res.send(`Hello ${req.body.name}!`);
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     const { name } = req.params;
+//     res.send(`Hello ${name}`);
+// });
 
 app.listen(8000, ()=> {
     console.log('server is listening on port 8000')
