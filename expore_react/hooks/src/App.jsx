6c0a5e8dff59252c26b5785caa344c4a28b5a4cb
mszahan <mszahan1@gmail.com>
2024-01-20 23:54:59 +0600

    react project setup

---
+++ expore_react/hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import {createRoot} from 'react-dom/client';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hola from the react</h1>
+        </div>
+    )
+}
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>)
