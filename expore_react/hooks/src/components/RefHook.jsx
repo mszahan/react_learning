dd03319881f172ca960d612bcdfb80b65b9c8cf7
mszahan <mszahan1@gmail.com>
2024-01-21 23:29:14 +0600

    converted the uncontrolled form into controlled form with useState

--- expore_react/hooks/src/components/RefHook.jsx
+++ expore_react/hooks/src/components/RefHook.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 
 function ColorRef () {
@@ -16,6 +16,7 @@ function ColorRef () {
 
     return (
         <div className="ref-form">
+            <p>uncontrolled form with useRef</p>
             <form onSubmit={handleSubmit}>
 
                 <input type="text" 
@@ -32,11 +33,47 @@ function ColorRef () {
     )
 }
 
+
+function ControlForm () {
+    const [ sound, setSound]= useState();
+    const [ color, setColor ] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`${sound} sounds like ${color}`);
+        setSound('')
+        setColor('#000')
+    }
+
+    return (
+        <div className="ref-form">
+            <p>controlled form with useState</p>
+            <form onSubmit={handleSubmit}>
+
+                <input type="text" 
+                value={sound}
+                onChange={e => setSound(e.target.value)}
+                placeholder="sound.." />
+
+                <input type="color"
+                onChange={e => setColor(e.target.value)}
+                value={color}
+                />
+
+                <button>add</button>
+            </form>
+        </div>
+    )
+}
+
+
+
 const RefHook = () => {
     return (
         <div className="refhook">
             <h1>exploring the useRef hook</h1>
             <ColorRef/>
+            <ControlForm/>
         </div>
     )
 }
