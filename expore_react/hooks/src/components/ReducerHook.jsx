b12fb679f4770e932213ad96da53e2ce88c5eb56
mszahan <mszahan1@gmail.com>
2024-01-21 21:46:02 +0600

    implemented the increment with usereducer

--- expore_react/hooks/src/components/ReducerHook.jsx
+++ expore_react/hooks/src/components/ReducerHook.jsx
@@ -1,22 +1,66 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 
-const ReducerHook = () => {
+
+function Increament () {
+
     const [number, setNumber] = useReducer(
         (number, newNumber) => number + newNumber,
         0
     );
 
+
     return (
         <div className="reducer">
-            <h1>Exploring useReducer Hook</h1>
-
             <h4> {number} </h4>
             <button 
             onClick={() => setNumber(1)}
-            >increase</button>
+            >increase 1</button>
+
+            <button
+            onClick={() => setNumber(5)}
+            >increase 5</button>
+
+            <button onClick={() => setNumber(-2)}>
+                reduce 2
+            </button>
+        </div>
+    )
+}
 
 
+
+function CBox () {
+    // const [checked, setChecked] = useState(false);
+    const [checked, toggle] = useReducer(
+        checked => !checked,
+        false
+    )
+
+    return(
+        <div className="checkbox">
+            <input type="checkbox" 
+            onClick={toggle}
+            value={checked}
+            />
+                {
+                    checked
+                    ? <p>Checked </p>
+                    : <p>Unchecked</p>
+                }
+        </div>
+    )
+}
+
+
+
+
+const ReducerHook = () => {
+    return (
+        <div className="reducer-hook">
+            <h1>Exploring useReducer Hook</h1>
+            <Increament/>
+            <CBox/>
         </div>
     )
 }
