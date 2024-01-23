99594536b55b54496db1a1dba031d1187ebfc498
mszahan <mszahan1@gmail.com>
2024-01-23 20:00:37 +0600

    added the final version of this project

--- improve_react/navigation/src/App.jsx
+++ improve_react/navigation/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Categories from "./components/Categories";
+import Category from "./components/Category";
+import Header from "./components/Header";
+
+// import Home from "./components/HomeHome";
+// import About from "./About";
+// import Categories from "./Categories";
+// import Category from "./Category";
+// import Header from "./Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
+        <Route path="about" element={<About />} />
+        <Route path="categories" element={<Categories />}>
+          <Route path=":catId" element={<Category />} />
+        </Route>
+        <Route
+          path="*"
+          element={<h1 className="not-found">Page Not Found</h1>}
+        />
+      </Routes>
+
+      <footer className="container">
+        &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ improve_react/navigation/src/components/About.jsx
@@ -0,0 +1,21 @@
+export default function About() {
+    return (
+      <div className="container">
+        <h1>Technology and Business Solutions</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Nihil dolores fugit cumque enim
+           facere dolorem natus nesciunt quisquam, 
+          ad ullam quo ratione excepturi ipsam dicta hic repudiandae, officiis laboriosam nulla.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Culpa repellat accusantium dolorum rem cupiditate corrupti reprehenderit beatae atque perferendis, 
+          aspernatur nulla suscipit reiciendis fugiat soluta corporis ea! Nam voluptas iure corrupti nemo excepturi 
+          molestias et earum. 
+          Quidem expedita quaerat quis sconsequatur at ducimus delectus sequi repellat. 
+          Asperiores quisquam quia magnam!
+        </p>
+      </div>
+    );
+  }
