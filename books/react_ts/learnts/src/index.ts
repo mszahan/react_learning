d2646f07bf82e5d77c3d121c879634d54716b317
mszahan <mszahan1@gmail.com>
2024-01-25 17:00:38 +0600

    explored function

--- books/react_ts/learnts/src/index.ts
+++ books/react_ts/learnts/src/index.ts
@@ -23,6 +23,21 @@ let user: [number, string] = [4, 'rebecca'];
 
 
 // enum 
+// if you use const like this const enum Size
+// then js will be really short
 enum Size {Small =1, Medium, Large};
 let mySize = Size.Medium;
 console.log(mySize)
+
+
+// Functions
+
+function calculatePrice(unitPrice: number, tax:number): number {
+
+    if (unitPrice < 250){
+        return unitPrice * 1.5 +tax;
+    }
+    return unitPrice * 3.5 + tax;
+}
+
+calculatePrice(55, 120)
