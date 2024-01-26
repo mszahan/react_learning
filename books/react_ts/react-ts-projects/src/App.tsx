93b716e1ae43c913f622580e7fe749bff5a5f054
mszahan <mszahan1@gmail.com>
2024-01-26 18:18:44 +0600

    configured the prettier

--- books/react_ts/react-ts-projects/src/App.tsx
+++ books/react_ts/react-ts-projects/src/App.tsx
@@ -1,14 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <h1>Hello from the revite with ts</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
