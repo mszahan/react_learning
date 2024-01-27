2aea6412097f41a7c3913eef86eab69d59945fad
mszahan <mszahan1@gmail.com>
2024-01-27 22:23:18 +0600

    updated the personscore from state to reducer

--- books/react_ts/react-ts-projects/src/components/hooks/PersonScore.tsx
+++ books/react_ts/react-ts-projects/src/components/hooks/PersonScore.tsx
@@ -1,5 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { getPerson } from './getPerson';
+
+
+type State = {
+  name: string | undefined;
+  score: number;
+  loading: boolean;
+};
+
+type Action = 
+  | {
+    type: 'initialize';
+    name: string;
+  }
+
+  | {
+    type: 'increment';
+  }
+
+  |{
+    type: 'decrement';
+  }
+  
+  | {
+    type: 'reset';
+  };
+
+
+function reducer (state: State, action : Action): State {
+  switch (action.type) {
+    case 'initialize':
+      return { name: action.name, score:0, loading: false}
+    case 'increment':
+      return {...state, score: state.score +1};
+    case 'decrement':
+      return {...state, score: state.score -1};
+    case 'reset':
+      return {...state, score:0};
+    default:
+      return state;
+  }
+  
+}
+
+
 
 export function PersonScore() {
   // you cannot use the async await function directly within the useEffect
@@ -10,14 +54,22 @@ export function PersonScore() {
   // }
   // getThePerson();
 
-  const [name, setName] = useState<string | undefined>();
-  const [score, setScore] = useState(0);
-  const [loading, setLoading] = useState(true);
+  // const [name, setName] = useState <string | undefined> ();
+  // const [score, setScore] = useState(0);
+  // const [loading, setLoading] = useState(true);
+
+  const [{name, score, loading}, dispatch] = useReducer(
+    reducer,
+    {
+      name:undefined,
+      score: 0,
+      loading: true,
+    }
+  )
 
   useEffect(() => {
-    getPerson().then((person) => {
-      setLoading(false);
-      setName(person.name);
+    getPerson().then(({name}) => {
+      dispatch({type: 'initialize', name})
     })
     
   }, []);
@@ -31,9 +83,9 @@ export function PersonScore() {
       <h3>
         {name} - score: {score}
       </h3>
-      <button onClick={() => setScore(score+1)}>Add</button>
-      <button onClick={() => setScore(score-1)}>Submit</button>
-      <button onClick={() => setScore(0)}>Reset</button>
+      <button onClick={() => dispatch({type: 'increment'})}>Add</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>Subtract</button>
+      <button onClick={() => dispatch({type:'reset'})}>Reset</button>
     </div>
   );
 }
