b54cabe7f5b1db9eaac92a15693f83aa4e3343ce
mszahan <mszahan1@gmail.com>
2024-01-29 21:28:34 +0600

    replaced the navigate with Form navigatio

--- books/react_ts/react-ts-projects/src/components/Header.tsx
+++ books/react_ts/react-ts-projects/src/components/Header.tsx
@@ -1,28 +1,29 @@
-import { FormEvent } from "react";
-import { NavLink, Link, useSearchParams, useNavigate } from "react-router-dom";
+// import { FormEvent } from "react";
+import { NavLink, Link, useSearchParams, Form } from "react-router-dom";
 import logo from '../assets/react.svg'
 
 export function Header() {
     const [searchParams] = useSearchParams();
     // const [searchParams, setSearchParams] = useSearchParams();
-    const navigate = useNavigate();
-    function handleSearchSubmit (e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        // use the JavaScript FormData interface to get the value of the search field
-        const formData = new FormData (e.currentTarget);
+    // const navigate = useNavigate();
+    // function handleSearchSubmit (e: FormEvent<HTMLFormElement>) {
+    //     e.preventDefault();
+    //     // use the JavaScript FormData interface to get the value of the search field
+    //     const formData = new FormData (e.currentTarget);
 
-        // use a type assertion to set the type of the search field value to a string
-        const search = formData.get('search') as string;
+    //     // use a type assertion to set the type of the search field value to a string
+    //     const search = formData.get('search') as string;
 
-        // setSearchParams({search})
-        navigate(`/products/?search=${search}`)
-    }
+    //     // setSearchParams({search})
+    //     navigate(`/products/?search=${search}`)
+    // }
 
     return (
         <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
-            <form 
+            <Form 
             className="relative text-right"
-            onSubmit={handleSearchSubmit}
+            action="/products"
+            // onSubmit={handleSearchSubmit}
             >
                 <input 
                 type="search" 
@@ -32,7 +33,7 @@ export function Header() {
                 className="absolute right-0 top-0 rounded py-2 px-3 text-gray-700"
                 />
 
-            </form>
+            </Form>
             <Link to='/'>
                 <img 
                 src={logo}
