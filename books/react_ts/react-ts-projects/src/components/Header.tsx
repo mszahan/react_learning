086642749bda1a0d62ae358675493e8c617a9681
mszahan <mszahan1@gmail.com>
2024-01-29 19:47:58 +0600

    implemented the search with useSearchParams
    it's working like normal web page with now update within page

--- books/react_ts/react-ts-projects/src/components/Header.tsx
+++ books/react_ts/react-ts-projects/src/components/Header.tsx
@@ -1,9 +1,35 @@
-import { NavLink, Link } from "react-router-dom";
+import { FormEvent } from "react";
+import { NavLink, Link, useSearchParams } from "react-router-dom";
 import logo from '../assets/react.svg'
 
 export function Header() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    function handleSearchSubmit (e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        // use the JavaScript FormData interface to get the value of the search field
+        const formData = new FormData (e.currentTarget);
+
+        // use a type assertion to set the type of the search field value to a string
+        const search = formData.get('search') as string;
+        
+        setSearchParams({search})
+    }
+
     return (
         <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
+            <form 
+            className="relative text-right"
+            onSubmit={handleSearchSubmit}
+            >
+                <input 
+                type="search" 
+                name="search" 
+                placeholder="Search"
+                defaultValue={searchParams.get('search') ?? ''}
+                className="absolute right-0 top-0 rounded py-2 px-3 text-gray-700"
+                />
+
+            </form>
             <Link to='/'>
                 <img 
                 src={logo}
@@ -15,7 +41,7 @@ export function Header() {
             <Link to=''>
                 <h1 className="text-2xl">React Tools</h1>
             </Link>
-            
+
             <nav>
                 <NavLink 
                 to='products'
--- books/react_ts/react-ts-projects/src/pages/ProductsPage.tsx
+++ books/react_ts/react-ts-projects/src/pages/ProductsPage.tsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { products } from "../data/products";
 
 export function ProductsPage() {
+    const [searchParams] = useSearchParams();
+
+    function getFilteredProducts() {
+        const search = searchParams.get('search');
+        if (search === null || search === '') {
+            return products;
+        } else {
+            return products.filter(
+                product => product.name.toLowerCase()
+                .indexOf(search.toLocaleLowerCase()) > -1
+            )
+        }
+    }
+
     return (
         <div className="text-center p-5">
             <h2 className="text-xl font-bold text-slate-600">
@@ -9,7 +23,7 @@ export function ProductsPage() {
             </h2>
             <ul className="list-none m-0 p-0">
                 {
-                    products.map(product => (
+                    getFilteredProducts().map(product => (
                         <li key={product.id}>
                             <Link 
                             to={`${product.id}`}
