67d4aa5d2a789b6aeda03c63bb442231499d1d87
mszahan <mszahan1@gmail.com>
2024-02-08 17:49:32 +0600

    implemented getRepo method

--- books/react_ts/react-ts-projects/src/api/types.ts
+++ books/react_ts/react-ts-projects/src/api/types.ts
@@ -2,3 +2,20 @@ export type ViewerData = {
     name: string;
     avatarUrl: string;
 };
+
+export type SearchCriteria = {
+    org: string;
+    repo: string;
+};
+
+export type RepoData = {
+    repository: {
+        id: string;
+        name: string;
+        description: string;
+        viewerHasSatrred: boolean;
+        stargazers: {
+            tottalCount: number;
+        };
+    };
+};
