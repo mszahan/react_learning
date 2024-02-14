4d2b364700807534bad1df84daddd3251cb76f57
mszahan <mszahan1@gmail.com>
2024-02-14 17:37:55 +0600

    added the checkbox next to each list

--- books/react_ts/react-ts-projects/src/checklist/Checklist.tsx
+++ books/react_ts/react-ts-projects/src/checklist/Checklist.tsx
@@ -1,4 +1,5 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode, useState } from 'react';
+import { IdValue } from './types';
 
 type Props<Data> = {
     data: Data[];
@@ -16,6 +17,14 @@ export function Checklist<Data>({
     renderItem,
     ...urlProps
 }: Props<Data>) {
+    const [checkedIds, setCheckedIds] = useState<IdValue[]>([]);
+    const handleCheckChange = (checkedId: IdValue) => () => {
+        const isChecked = checkedIds.includes(checkedId);
+        const newCheckedIds = isChecked
+            ? checkedIds.filter((itemCheckedid) => itemCheckedid !== checkedId)
+            : checkedIds.concat(checkedId);
+        setCheckedIds(newCheckedIds);
+    };
     return (
         <ul className="bg-gray-300 rounded p-10" {...urlProps}>
             {data.map((item) => {
@@ -37,10 +46,19 @@ export function Checklist<Data>({
 
                 return (
                     <li key={idValue} className="bg-white p-6 shadow rounded mb-4 last:mb-0">
-                        <div className="text-xl text-gray-800 pb-1">{primaryText}</div>
-                        {typeof secondaryText === 'string' && (
-                            <div className="text-sm text-gray-500">{secondaryText}</div>
-                        )}
+                        <label className="flex item-center">
+                            <input
+                                type="checkbox"
+                                checked={checkedIds.includes(idValue)}
+                                onChange={handleCheckChange(idValue)}
+                            />
+                            <div className="ml-2">
+                                <div className="text-xl text-gray-800 pb-1">{primaryText}</div>
+                                {typeof secondaryText === 'string' && (
+                                    <div className="text-sm text-gray-500">{secondaryText}</div>
+                                )}
+                            </div>
+                        </label>
                     </li>
                 );
             })}
--- books/react_ts/react-ts-projects/src/checklist/ChecklistPage.tsx
+++ books/react_ts/react-ts-projects/src/checklist/ChecklistPage.tsx
@@ -7,16 +7,16 @@ function ChecklistPage() {
                 data={[
                     { id: 1, name: 'Alex', role: 'Assistant' },
                     { id: 2, name: 'Reb', role: 'Dev' },
-                    { id: 2, name: 'Meg', role: 'manger' },
-                    { id: 2, name: 'Jen', role: 'director' },
-                    { id: 2, name: 'Jo', role: 'Employee' },
+                    { id: 3, name: 'Meg', role: 'manger' },
+                    { id: 4, name: 'Jen', role: 'director' },
+                    { id: 5, name: 'Jo', role: 'Employee' },
                 ]}
-                renderItem={(item) => (
-                    <li key={item.id} className="bg-white p-4 border-b-2">
-                        <div className="tex-xl text-slate-800 pb-1">{item.name}</div>
-                        <div className="text-slate-500">{item.role}</div>
-                    </li>
-                )}
+                // renderItem={(item) => (
+                //     <li key={item.id} className="bg-white p-4 border-b-2">
+                //         <div className="tex-xl text-slate-800 pb-1">{item.name}</div>
+                //         <div className="text-slate-500">{item.role}</div>
+                //     </li>
+                // )}
                 id="id"
                 primary="name"
                 secondary="role"
