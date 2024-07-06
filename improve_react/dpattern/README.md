d3f9737ca170898ecac4d0d659f57905aecfd82b
mszahan <mszahan1@gmail.com>
2024-07-06 11:48:55 +0600

    added new project for design pattern course

---
+++ improve_react/dpattern/README.md
@@ -0,0 +1,3 @@
+# React design pattern from linkedin learning
+
+all the code from the linkedin learning course 'React design pattern'
---
+++ improve_react/dpattern/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Let's learn react desing pattern</h1>
+    </>
+  );
+}
+
+export default App;
