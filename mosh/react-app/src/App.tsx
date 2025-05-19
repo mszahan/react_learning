a1b662eaad01630d60515e0c65e83ace34a9100c
mszahan <mszahan1@gmail.com>
2025-05-19 16:52:24 +0600

    created new react project with bootstrap installed and applied

---
+++ mosh/react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message"
+
+function App() {
+  return(
+    <div className="main">
+      <Message/>
+    </div>
+  )
+}
+
+export default App;
---
+++ mosh/react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    return(
+        <h1>Hello world</h1>
+    )
+}
+
+export default Message;
