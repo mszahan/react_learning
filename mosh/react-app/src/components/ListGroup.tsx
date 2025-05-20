19b655307314e538c8f4ae46cd129a6bb16c3a5f
mszahan <mszahan1@gmail.com>
2025-05-20 12:25:29 +0600

    manged state in react

--- mosh/react-app/src/components/ListGroup.tsx
+++ mosh/react-app/src/components/ListGroup.tsx
@@ -1,4 +1,5 @@
-import type { MouseEvent } from "react";
+// import type { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const items = [
@@ -11,16 +12,26 @@ function ListGroup() {
     "Philadelphia",
   ];
 
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List group</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
