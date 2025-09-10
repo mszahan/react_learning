c29d596109b32bb278a3189b83fe87ba15f9b3de
mszahan <mszahan1@gmail.com>
2025-09-10 13:18:21 +0600

    did optimistic deletion with jsonplaceholder data

--- mosh/react-app/src/components/connectingBackend/FetchingData.tsx
+++ mosh/react-app/src/components/connectingBackend/FetchingData.tsx
@@ -25,15 +25,38 @@ const FetchingData = () => {
     //you could just do this but strict mode doesn't support here
     // .finally(()=> setLoading(false))
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <div className="json">
       <p className="mt-5">fetching data from json placeholder</p>
       {loading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
 
-      <ul>
+      <ul className="list-group w-25">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.id} - {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
