f658e0a2cb4a398ceca364a4ed984eb7440cde9b
mszahan <mszahan1@gmail.com>
2025-09-14 11:50:37 +0600

    built a responsive layout with chakra ui

--- mosh/game-hub/src/App.tsx
+++ mosh/game-hub/src/App.tsx
@@ -1,8 +1,25 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" hideBelow="lg">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
