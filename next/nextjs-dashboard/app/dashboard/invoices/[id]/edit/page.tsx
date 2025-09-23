25ab0127276caa8bf3cae51f0992b416a4dc5bfb
mszahan <mszahan1@gmail.com>
2025-09-23 11:57:47 +0600

    added not found page for edit endpoint of invoice

--- next/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ next/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -9,6 +10,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
