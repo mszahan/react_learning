fb6b7b0dfe48e4e84f4544a2288c292508650781
mszahan <mszahan1@gmail.com>
2025-09-24 15:29:54 +0600

    added metadata

--- next/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ next/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 export default async function Page(props: {
   searchParams?: Promise<{
