de7c3f580b43442da7db144b850b132218621daf
mszahan <mszahan1@gmail.com>
2025-09-24 18:15:22 +0600

    added tailwind navbar component to new nextjs project

---
+++ next/getting-started-docs/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center m-5 font-bold">Hello world</h1>
+    </div>
+  );
+}
---
+++ next/getting-started-docs/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com", "tailwindcss.com"],
+  },
+};
+
+export default nextConfig;
