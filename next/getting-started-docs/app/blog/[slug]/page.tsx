cc514b08c8c839c77c2c3b07ed779269901e1cb8
mszahan <mszahan1@gmail.com>
2025-10-07 21:11:10 +0600

    added dynamic segments with slug

--- next/getting-started-docs/app/blog/[slug]/page.tsx
+++ next/getting-started-docs/app/blog/[slug]/page.tsx
@@ -1,5 +1,11 @@
-function generateStaticParams() {}
+export default async function BlogPostPage ({params}: {params: Promise<{slug: string}>}) {
+    const {slug} = await params
 
-export default function Page() {
-    return <h1>hello blog post page</h1>
+    return (
+        <div className="blog-detail">
+            <h1>Title of the article</h1>
+            <h1> {slug} of the article</h1>
+            <p>Content of the blog</p>
+        </div>
+    )
 }
